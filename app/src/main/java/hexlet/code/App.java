0b6bea4d8a1593eb3275db10142ba0f6ab08c4ec
V. Shubina <victoriya.shubina.mex@gmail.com>
2023-11-14 13:59:03 +0300

    add help logic

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,29 @@
 package hexlet.code;
 
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import java.util.concurrent.Callable;
+
+@Command(name = "genDiff", mixinStandardHelpOptions = true, version = "version",
+        description = "Compares two configuration files and shows a difference.")
+class GenDiff implements Callable<Integer> {
+
+    @Option(names = {"-h", "--help"}, usageHelp = true, description = "Show this help message and exit.")
+    private boolean helpRequested = false;
+
+    @Option(names = {"-V", "--version"}, description = "Print version information and exit.")
+    boolean version;
+
+    @Override
+    public Integer call() throws Exception {
+        return null;
+    }
+}
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int exitCode = new CommandLine(new GenDiff()).execute(args);
+        System.exit(exitCode);
     }
 }
