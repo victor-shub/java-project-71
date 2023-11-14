773df3a2350ec2ae0010184eebb4349a03727901
V. Shubina <victoriya.shubina.mex@gmail.com>
2023-11-14 14:43:20 +0300

    add paths and format

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -3,11 +3,23 @@ package hexlet.code;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 import java.util.concurrent.Callable;
+import java.io.File;
 
 @Command(name = "genDiff", mixinStandardHelpOptions = true, version = "version",
         description = "Compares two configuration files and shows a difference.")
 class GenDiff implements Callable<Integer> {
+
+    @Parameters(paramLabel = "filepath1", description = "path to first file")
+    private File file1;
+
+    @Parameters(paramLabel = "filepath2", description = "path to second file")
+    private File file2;
+
+    @Option(names = {"-f", "--format"}, defaultValue = "stylish",
+            description = "output format [default: ${DEFAULT-VALUE}]", paramLabel = "format")
+    String format;
 
     @Option(names = {"-h", "--help"}, usageHelp = true, description = "Show this help message and exit.")
     private boolean helpRequested = false;
