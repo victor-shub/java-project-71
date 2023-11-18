6a298e962bbec352c536053b53124290cf3a3826
V. Shubina <victoriya.shubina.mex@gmail.com>
2023-11-18 17:11:05 +0300

    add objectmapper dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,6 +15,7 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     implementation("info.picocli:picocli:4.7.5")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-xml:2.16.0")
 }
 
 tasks.test {
